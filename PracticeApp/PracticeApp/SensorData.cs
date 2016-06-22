8fb3f71d34c5d5c9826cd47c5ced3da962506371
Subhendu De <subhendude@gmail.com>
2016-06-22 11:57:17 +0530

    Version 1 Commited

---
+++ PracticeApp/PracticeApp/SensorData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApp.Models
+{
+    public class SensorData
+    {
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+
+        public string TemperatureDisplay
+        {
+            get
+            {
+                return string.Format("{0:0.0} °C", this.Temperature);
+            }
+        }
+
+        public string HumidityDisplay
+        {
+            get
+            {
+                return string.Format("{0:0.0} % RH", this.Humidity);
+            }
+        }
+    }
+}
